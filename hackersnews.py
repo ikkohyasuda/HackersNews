f5f2b43ad43415c5a6cd3e0b68cb119c63746577
ikkohyasuda <yasudaikkoh@gmail.com>
2026-01-21 09:15:46 +0900

    first submission

---
+++ hackersnews.py
@@ -0,0 +1,34 @@
+import time
+import requests
+
+toppage_url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+each_url = "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty"
+
+
+def get_toppage_ids():
+    response = requests.get(toppage_url)
+    ids = response.json()  # .jsonの処理は文字列をリストへ変換するのに必要
+    return ids[:30]
+
+
+def get_item(item_id):
+    response = requests.get(each_url.replace("{}", str(item_id)))
+    return response.json()
+
+
+def list_titlelink():
+    id_groups = get_toppage_ids()
+
+    for item_id in id_groups:
+        time.sleep(1)  # 連続アクセス防止のために1秒待つ
+
+        item = get_item(item_id)
+
+        title = item.get("title")  # .get()を使用すると、対応するキーがない場合は"None"と表示される
+        link = item.get("url")
+
+        print(f"{{'title': '{title}', 'link': '{link}'}}")  # "{{"や"}}"で"{"や"}"は文字としての処理となる
+        # data = {"title": title, "link": link}とdictにしてもできる
+
+
+list_titlelink()
